c49a2e018aa8078a0297bf551278468c9246e3bc
Dalvan hoffmann wrzesmska <dalvanhoffmannwrzesmska@gmail.com>
2025-09-06 16:41:21 -0300

    Correção de callback jupiter

--- routes/web.php
+++ routes/web.php
@@ -178,6 +178,7 @@ Route::get('checkout/produto/v2/{id}', [CheckoutControlller::class, 'v2'])->wher
 require __DIR__ . '/auth.php';
 require __DIR__ . '/groups/adquirentes/cashtime.php';
 require __DIR__ . '/groups/adquirentes/podpay.php';
+require __DIR__ . '/groups/adquirentes/jupiterpay.php';
 Route::get('/teste', function () {
     return view('test');
 });
