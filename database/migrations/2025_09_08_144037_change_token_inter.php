51644bcc570717ede5c6512bb30088c9abd31225
Dalvan hoffmann wrzesmska <dalvanhoffmannwrzesmska@gmail.com>
2025-09-08 14:48:14 -0300

    Fix: alteração de coluna

--- database/migrations/2025_09_08_144037_change_token_inter.php
+++ database/migrations/2025_09_08_144037_change_token_inter.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table("token_inter", function (Blueprint $table) {
-            $table->string("refresh_token")->nullable();
+            $table->string("refresh_token")->nullable()->change();
         });
     }
 
